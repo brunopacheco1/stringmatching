42d7c0cb799f8c08abbf5fe5a794a763d0edd093
Bruno Pacheco <brunopacheco1@yahoo.com>
2016-12-05 21:16:10 -0200

    Ajuste de gráfico.

--- src/ru/fuzzysearch/LineChart.java
+++ src/ru/fuzzysearch/LineChart.java
@@ -1,15 +1,11 @@
 package ru.fuzzysearch;
 
-import java.awt.Color;
 import java.util.List;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.plot.XYPlot;
-import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
@@ -28,18 +24,18 @@ public class LineChart extends ApplicationFrame {
             dataset.addSeries(series);
         }
         
-        final JFreeChart chart = createChart(dataset);
+        final JFreeChart chart = createChart(title, dataset);
         final ChartPanel chartPanel = new ChartPanel(chart);
         chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
         setContentPane(chartPanel);
 
     }
 	
-	private JFreeChart createChart(final XYDataset dataset) {
+	private JFreeChart createChart(String title, XYDataset dataset) {
         
         // create the chart...
         final JFreeChart chart = ChartFactory.createXYLineChart(
-            "Line Chart Demo 6",      // chart title
+        	title,      // chart title
             "Tamanho do Dicionário",                      // x axis label
             "Tempo (ms)",                      // y axis label
             dataset,                  // data
@@ -49,21 +45,6 @@ public class LineChart extends ApplicationFrame {
             false                     // urls
         );
 
-//        chart.setBackgroundPaint(Color.white);
-//
-//        final XYPlot plot = chart.getXYPlot();
-//        plot.setBackgroundPaint(Color.lightGray);
-//        plot.setDomainGridlinePaint(Color.white);
-//        plot.setRangeGridlinePaint(Color.white);
-//        
-//        final XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
-//        renderer.setSeriesLinesVisible(0, false);
-//        renderer.setSeriesShapesVisible(1, false);
-//        plot.setRenderer(renderer);
-//
-//        final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
-//        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
-                
         return chart;
         
     }
